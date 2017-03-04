b68896907d69405c1608afc665692290cac11fc8
Adolfo De Unánue <nanounanue@gmail.com>
2017-03-04 00:16:03 -0600

    Script de descarga

---
+++ get_rita.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+""" get_rita.py
+"""
+
+import datetime
+from calendar import month_name
+import requests
+import click
+
+import logging
+import logging.config
+logging.config.fileConfig('logging.cfg')
+logger = logging.getLogger('rita')
+
+
+from time import clock
+
+from post_data import POST_DATA
+
+## Some constants needed in the request's header 
+HOSTNAME = "www.transtats.bts.gov"
+TRANSTAT_URL = "https://{host}/DownLoad_Table.asp?" + \
+               "Table_ID=236&Has_Group=3&Is_Zipped=0"
+REFERER="https://{host}/DL_SelectFields.asp?Table_ID=236"
+ORIGIN="https://{host}"
+USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/55.0.2883.87 Chrome/55.0.2883.87 Safari/537.36"
+CONTENT_TYPE="application/x-www-form-urlencoded"
+
+## Data directory
+DATA_DIR="data"
+
+def _valid_date(year, month):
+    is_valid = True
+    ## the available data set is generally about 3 months behind, make
+    ## sure that we never ask for data that is within the last 3 months
+    max_date = datetime.date.today() + datetime.timedelta(days=-90)
+
+    max_year = int(max_date.strftime('%Y'))
+    max_month = int(max_date.strftime('%m'))
+
+    if year > max_year:
+        logging.info("You are requesting data from the future!: {} should be less or equal than {}".format(year, max_year))
+        is_valid = False
+
+    if year == max_year and month > max_month:
+        logging.info("You are requesting data from the future!: {}/{} should be less than {}/{}".format(month, year, max_month, max_year))
+        is_valid = False
+
+    return is_valid 
+
+@click.command()
+@click.option('--year', help='Year to be downloaded', type=click.INT)
+@click.option('--month', help='Month to be downloaded', type=click.INT)
+def download_data(year, month):
+    """
+    """
+
+    if not _valid_date(year, month):
+        raise Exception("It is probable that this data ({}/{}) doesn't exists yet ".format(month, year))
+
+    logger.info("Collecting RITA for: {}/{}".format(month, year))
+
+    ## Fixing the data to POST
+    ## For some weird reason, FREQUENCY is equal to the month ... (¬_¬)
+    post_data = POST_DATA.format(year=year, month_name=month_name[month], month=month, frequency=month)
+
+    ## Fixing EOLs
+    post_data = post_data.replace("\n", "")
+
+    ## Make a friendly file name
+    output_file_name = '{0}/{1}-{2}'.format(DATA_DIR,str(month).zfill(2), year)
+    zip_file_name = '{}.zip'.format(output_file_name)
+
+    ## Create a session object, so we can keep the cookies, headers, etc. between requests
+    with requests.Session() as s:
+        ## Get the cookie and a good header from the server
+        host = TRANSTAT_URL.format(host=HOSTNAME)
+        r = s.get(host)
+
+        ## Add info to the session's headers
+        s.headers['User-Agent'] = USER_AGENT
+        s.headers['Referer'] = REFERER.format(host=HOSTNAME)
+        s.headers['Origin'] = ORIGIN.format(host=HOSTNAME)
+        s.headers['Content-Type'] = CONTENT_TYPE
+
+        logger.info("Sending POST to {}".format(host))
+
+        tic = clock()
+        ## We don't want to follow the redirect
+        response = s.post(host, data=post_data, allow_redirects=False)
+
+        if not response.status_code == 302:
+            raise Exception("Request was not successful with responde {}".format(response.status_code))
+
+        ## The Location of the file created for us is in the header in 'Location'
+        remote_file = response.headers['Location']
+
+        response = s.get(remote_file)
+
+        ## Write the remote file to local disk
+        with open(zip_file_name, "wb") as local_file:
+            local_file.write(response.content)
+
+        tac = clock()
+
+        logger.info("Downloaded: {} in {} seconds".format(zip_file_name, tac-tic))
+
+
+if __name__ == '__main__':
+    download_data()
